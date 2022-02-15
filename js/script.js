6714c6a388cc145e2bbb167a9cc8ebbefa44f81e
llsahakyanll <88764785+llsahakyanll@users.noreply.github.com>
2022-02-15 15:15:27 +0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 AOS.init();
 
+let div = document.querySelector(".firstBlock_main");
 document.addEventListener("mousemove", function(e){
     this.querySelectorAll('.layer').forEach(layer => {
         const speed = layer.getAttribute('data-speed');
 
-        const x = (window.innerWidth - e.pageX*speed)/100; 
-        const y = (window.innerHeight - e.pageY*speed)/100;
+        const x = (div.innerWidth - e.pageX*speed)/100; 
+        const y = (div.innerHeight - e.pageY*speed)/100;
 
         layer.style.transform = `translateX(${x}px) translateY(${y}px)`
     })
